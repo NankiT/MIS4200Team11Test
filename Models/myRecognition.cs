5b4d4cb1353053d9e23225a224497189cad72f68
Nicholas Nankivell <nn605016@ohio.edu>
2019-11-04 11:38:57 -0500

    Played with database connections

--- Models/myRecognition.cs
+++ Models/myRecognition.cs
@@ -13,7 +13,7 @@ namespace MIS4200Team11.Models
         [Display(Name = "Subject")]
         public string recognitionTitle { get; set; }
 
-        [Display(Name = "What did he/she do that represented one of our values?")]
+        [Display(Name = "What did they do that represented one of our values?")]
         public string recognitionDescription { get; set; }
         [DataType(DataType.Date)]
         public DateTime recognitionDate { get; set; }
